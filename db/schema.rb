595cb488ced40cbfd2a8731e82f5930cfb6aea81
MacAir <MacAir@MacAirs-MacBook-Air.local>
2016-02-24 00:20:38 +0700

    add user_id to places in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223163623) do
+ActiveRecord::Schema.define(version: 20160223171957) do
 
   create_table "places", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160223163623) do
     t.string   "rating"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
