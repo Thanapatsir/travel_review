bf2a80c0d5dab174bd95ab96101f044f63f3d514
MacAir <MacAir@MacAirs-MacBook-Air.local>
2016-02-24 16:39:39 +0700

    scaffold another model called reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223184332) do
+ActiveRecord::Schema.define(version: 20160224093841) do
 
   create_table "places", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20160223184332) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
