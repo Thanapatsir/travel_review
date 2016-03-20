c5e2132310663bb1a7541362f0276fe776996547
MacAir <MacAir@MacAirs-MacBook-Air.local>
2016-03-21 02:04:44 +0700

    not done yet, sighh

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'travel_review'
+set :repo_url, 'git@github.com:Thanapatsir/travel_review.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
