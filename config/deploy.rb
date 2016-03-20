c45ddcd970cd26ae2601f46b5fc72d489970d9c8
MacAir <MacAir@MacAirs-MacBook-Air.local>
2016-03-21 02:02:50 +0700

    another done

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,9 +36,9 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 
 lock '3.4.0'
 set :application, 'travel_review'
-set :repo_url, 'https://github.com/Thanapatsir/travel_review.git'
+set :repo_url, 'git@github.com:Thanapatsir/travel_review.git'
 
-set :rvm_ruby_version, '2.1.5@travel_review'
+set :rvm_ruby_version, '2.2.1@travel_review'
 set :deploy_to, '/home/thanapat/travel_review'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
